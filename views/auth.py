c5c87d66d562914a656b297f52f2150a9a7142e7
Александр <120110463+Jaxyk@users.noreply.github.com>
2023-04-02 17:47:19 +0300

    Add files via upload

--- views/auth.py
+++ views/auth.py
@@ -1,34 +1,34 @@
 from flask import request
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, abort
 
 from container import auth_service, user_service
 
 auth_ns = Namespace('auth')
 
 
-@auth_ns.route('/login')
+@auth_ns.route('/login/')
 class AuthView(Resource):
     def post(self):
         data = request.json
-        email = data.get('email')
-        password = data.get('password')
-        if not all([email, password]):
-           return "", 400
+        email = data.get('email', None)
+        password = data.get('password', None)
+        if None in [email, password]:
+            abort(400)
 
-        token = auth_service.generate_tokens(email, password)
+        tokens = auth_service.generate_tokens(email, password)
 
-        return token, 201
+        return tokens, 200
 
     def put(self):
         data = request.json
         token = data.get('refresh_token')
         tokens = auth_service.approve_refresh_token(token)
-        return tokens, 201
+        return tokens, 200
 
-@auth_ns.route('/register')
+
+@auth_ns.route('/register/')
 class AuthView(Resource):
     def post(self):
         req_json = request.json
         user_service.create(req_json)
-        return "", 201
-
+        return "Ok", 201
